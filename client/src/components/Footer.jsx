d3c479fc205c10443ab403e7be5e00d20ad93e61
Janavi-S05 <janavisivakumar05@gmail.com>
2025-03-23 15:33:24 +0530

    Job Portal website

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='container px-4 2xl:px-20 mx-auto'>
+            <div className='relative bg-gradient-to-r from-violet-50 to-purple-50 p-5 rounded-lg'>
+                <div className='flex flex-col justify-center items-center'>
+                    <h1 className='text-violet-800'>Keep Going!!!</h1>
+                    <p className='text-gray-800'>@2025 | All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
