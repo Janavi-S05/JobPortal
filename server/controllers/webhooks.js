29492a2ee223f6c2fed160cf09f6b5bf75a52fc0
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2025-04-20 22:37:43 +0530

    Update webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -10,7 +10,7 @@ const clerkWebhooks = async(req,res) => {
             "svix-signature" : req.headers["svix-signature"]
         })
 
-        const {data,type} = req.body
+        const {data,type} = req.body;
         
         switch (type) {
             case 'user.created': {
@@ -21,7 +21,8 @@ const clerkWebhooks = async(req,res) => {
                     image: data.image_url,
                     resume: ''
                 }
-                await User.create(userData)
+                await User.create(userData);
+                console.log("✅ User created:", userData);
                 res.json({})
                 break;
             }
